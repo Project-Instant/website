5b58640455e86cb0def94c776d939885f4b6f0b0
distribate <fank.tomphson@gmail.com>
2025-03-24 02:14:05 +0300

    added image compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@tailwindcss/vite";
 import solidJs from '@astrojs/solid-js';
-
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -10,7 +10,40 @@ export default defineConfig({
   prefetch: true,
   vite: {
     plugins: [
-      tailwind()
+      tailwind(),
+      ViteImageOptimizer({
+        test: /\.(jpe?g|png|gif|tiff|webp|avif)$/i,
+        exclude: undefined,
+        include: undefined,
+        includePublic: true,
+        logStats: true,
+        ansiColors: true,
+        svg: {
+          multipass: false,
+          plugins: [],
+        },
+        png: {
+          quality: 100,
+        },
+        jpeg: {
+          quality: 100,
+        },
+        jpg: {
+          quality: 100,
+        },
+        tiff: {
+          quality: 100,
+        },
+        gif: {},
+        webp: {
+          lossless: true,
+        },
+        avif: {
+          lossless: true,
+        },
+        cache: false,
+        cacheLocation: undefined,
+      })
     ],
   },
   i18n: {
@@ -18,7 +51,10 @@ export default defineConfig({
     defaultLocale: "en",
   },
   site: "https://instant-website-beta.vercel.app",
-  integrations: [solidJs({ 
-    devtools: true 
-  }), sitemap()]
+  integrations: [
+    solidJs({ 
+      devtools: true 
+    }),
+    sitemap()
+  ]
 });
